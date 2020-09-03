fa33a5d4677ca4d1eef9d9decd1223cb56887e7e
Wisam Halawi <w_halawi@hotmail.co.uk>
2020-09-03 18:00:27 +0100

    Minor change to index to see netlify live deploy in progress

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,9 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">wisamhalawi</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">
+        Wisam Halawi's portfolio site. Currently in the works.
+      </h1>
     </div>
   </div>
 </template>

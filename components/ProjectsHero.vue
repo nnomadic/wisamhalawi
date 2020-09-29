6dd2c1e510a25b746ff77cf0a7ed258d2dca932e
DESKTOP-T0N93PN\Wis <w_halawi@hotmail.co.uk>
2020-09-29 18:54:57 +0100

    changed background image src for projects hero back to jpg, simple solution to safari and won't be intensive

--- components/ProjectsHero.vue
+++ components/ProjectsHero.vue
@@ -4,7 +4,7 @@
       class="hero-body"
       :style="{
         backgroundImage:
-          'url(' + require('@/assets/images/' + imageName + '.webp') + ')',
+          'url(' + require('@/assets/images/' + imageName + '.jpg') + ')',
       }"
     >
       <div class="container">
